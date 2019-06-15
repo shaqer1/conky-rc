ea4fa103736683dbf439d0fa7e12fabe649e30c7
Shafay Haq <haqs@purdue.edu>
2019-06-15 12:46:11 +0500

    added direction description fixed location issue

--- weather/weather.py
+++ weather/weather.py
@@ -2,7 +2,14 @@ import requests
 import json
 import urllib.request
 
-
+def windDegreeSwitcher(argument):
+    switcher = {
+        0: "East",
+        90: "North",
+        180: "West",
+        270: "South",
+    }
+    return switcher.get(argument, "Invalid Degrees")
 
 def downloader(image_url):
     urllib.request.urlretrieve(image_url,'/home/shafay/conkyConfigs/weather/weatherDesc.png')
@@ -18,11 +25,15 @@ def main():
     geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
     geo_request = requests.get(geo_request_url)
     lat = geo_request.json()['latitude']
+    lat = lat[0:-2]
+
     lon = geo_request.json()['longitude']
+    lon = lon[0:-2]
 
     # print(lat,lon)
 
     # print(lat, lon)
+    BASEURL = 'http://api.openweathermap.org/data/2.5/'
 
     PARAMS = {
         'appid': APIkey,
@@ -30,12 +41,14 @@ def main():
         'lon': lon
     }
 
-    currentWeatherData = requests.get(url = 'http://api.openweathermap.org/data/2.5/weather', params = PARAMS).json()
-    #print(currentWeatherData)
+    currentWeatherData = requests.get(url = BASEURL + 'weather', params = PARAMS).json()
+    # print(currentWeatherData)
     
     
     windSpeed = currentWeatherData['wind']['speed']
     windDegrees = currentWeatherData['wind']['deg']
+    windDegrees = windDegreeSwitcher(windDegrees)
+
 
     weatherDesc = currentWeatherData['weather'][0]['main']
 
@@ -60,7 +73,24 @@ def main():
 
     s = ""
     # temp = '{}'.format(
-    s = ('{}\n{}\n{}\n{} {} {}\n{} {}\n{} {}\n'.format(cityname, round(temp,0) + 'ºF', weatherDesc, pressure + 'hPa', humidity + '%', 'Clouds%:     ' + cloudsPerc + '%', 'MinTemp: ' + tempMin, 'MinTemp: ' + tempMax, windSpeed + 'm/s', windDegrees))
+    s = ('{}\n{}\n{}\n{} {}\n{}\n{} {}\n{} {}\n'.format(cityname, 
+        str(round(temp,0)) + 'ºF', 
+        weatherDesc,
+        'Pressure:  ' + str(pressure) + 'hPa       ',
+        'Humidity: ' + str(humidity) + '%     ',
+         'Clouds%:     ' + str(cloudsPerc) + '%',
+          'MinTemp:   ' + str(tempMin) + 'ºF', 
+          '     MaxTemp:     ' + str(tempMax) + 'ºF',
+           'Wind Speed: ' + str(windSpeed) + 'm/s     ',
+            'Direction: ' + str(windDegrees)))
+
+    # TODO get 3 day forecast
+
+    forecastData = requests.get(url = BASEURL + 'forecast/daily', params = PARAMS).json()
+
+
+    hourlyData = requests.get(url = BASEURL + 'forecast', params = PARAMS).json()
+
     
     f = open("/home/shafay/conkyConfigs/weather/weatherDat","w+")
 
