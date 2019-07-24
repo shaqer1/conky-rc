2f033c67e48cca57ecf7d2c5147b5c4bf37c5c0a
Shafay Haq <haqs@purdue.edu>
2019-07-24 11:12:39 +0500

    added colors to horoscope

--- conkyHoros/horo.sh
+++ conkyHoros/horo.sh
@@ -31,7 +31,7 @@ wget https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-tod
 wget https://www.horoscope.com/star-ratings/today/scorpio --output-document=scorpioStars.html -q
 function getDate {
     # date=$1
-    date=$(grep '<p><strong class="date">' scorpio.html | cut -c 25- | rev | grep -o -P ">gnorts/<.{0,}" | cut -c 10- | rev)
+    date=$(grep '<p><strong>' scorpio.html | cut -c 12- | rev | grep -o -P ">gnorts/<.{0,}" | cut -c 10- | rev)
     #echo "$date"
     year=$(echo "$date" | grep -o -P ', .{0,}' | cut -c 3-)
     day=$(echo "$date" | grep -o -P ' .{0,}' | cut -c 2- | rev | grep -o -P ' .{0,}' | cut -c 3- | rev)
@@ -65,7 +65,7 @@ date=$(getDate)
 systemDayPrint=$(date --date="@${date}" +"%a, %b %d")
 
 
-horoscope=$(grep '<p><strong class="date">' scorpio.html | cut -c 25- | rev | cut -c 5- | rev | grep -o -P '.{0}</strong>.*' | cut -c 13-) 
+horoscope=$(grep '<p><strong>' scorpio.html | cut -c 12- | rev | cut -c 5- | rev | grep -o -P '.{0}</strong>.*' | cut -c 13-) 
 echo "$systemDayPrint-$horoscope"  | fold -s -w 55 > /home/shafay/conkyConfigs/conkyHoros/horoscope
 grep "<h3>Today's Matches</h3>" scorpio.html -A 15 > matches
 grep "<h3>Sex <i" scorpioStars.html -A 8 > stars 
@@ -84,34 +84,34 @@ rating=$(cat stars | sed -n 1p | grep -o 'highlight' | wc -l)
 star='${alignc}'
 star=$(getStars $rating $star)
 description=$(cat stars | sed -n 2p | cut -c 4- | rev | cut -c 5- | rev | fold -s -w 55)
-echo 'Sex' "$star" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo '${color red}Sex' "$star \$color" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 echo "$description" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 
 rating=$(cat stars | sed -n 3p | grep -o 'highlight' | wc -l)
 star='${alignc}'
 star=$(getStars $rating $star)
 description=$(cat stars | sed -n 4p | cut -c 4- | rev | cut -c 5- | rev | fold -s -w 55)
-echo 'Hustle' "$star" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo '${color yellow}Hustle' "$star\$color" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 echo "$description" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 
 rating=$(cat stars | sed -n 5p | grep -o 'highlight' | wc -l)
 star='${alignc}'
 star=$(getStars $rating $star)
 description=$(cat stars | sed -n 6p | cut -c 4- | rev | cut -c 5- | rev | fold -s -w 55)
-echo 'Vibe' "$star" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo '${color blue}Vibe' "$star\$color" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 echo "$description" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 
 rating=$(cat stars | sed -n 7p | grep -o 'highlight' | wc -l)
 star='${alignc}'
 star=$(getStars $rating $star)
 description=$(cat stars | sed -n 8p | cut -c 4- | rev | cut -c 5- | rev | fold -s -w 55)
-echo 'Success' "$star" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo '${color green}Success' "$star\$color" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 echo "$description" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 
-echo '♥ ${alignc} ${font Symbola:size=8}🤝${alignr}💼${font DejaVu Sans Mono:size=9}                 ' >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo '${color red}♥${alignc}${color yellow}${font Symbola:size=8}🤝${alignr}${color #8B4513}💼${font DejaVu Sans Mono:size=9}' >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 match1=$(cat matches | sed -n 6p | cut -c 4- | rev | cut -c 5- | rev)
 match2=$(cat matches | sed -n 11p | cut -c 4- | rev | cut -c 5- | rev)
 match3=$(cat matches | sed -n 16p | cut -c 4- | rev | cut -c 5- | rev)
-echo "$match1${newmap["$match1"]}\${alignc}$match2${newmap["$match2"]}\${alignr}$match3${newmap["$match3"]}                 " >> /home/shafay/conkyConfigs/conkyHoros/horoscope
+echo "\${color red}$match1${newmap["$match1"]}\${alignc}\${color yellow}$match2${newmap["$match2"]}\${alignr}\${color #8B4513}$match3${newmap["$match3"]}" >> /home/shafay/conkyConfigs/conkyHoros/horoscope
 
 rm -f scorpio* matches stars wget-log*
