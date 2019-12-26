c4be819d9fa14641c64abb9d73023d11bf2ba50d
Shafay Haq <haqs@purdue.edu>
2019-12-26 07:55:07 -0500

    removed todo list from md file fixed issue with odd number of lists with no content

--- ListMD/execute.py
+++ ListMD/execute.py
@@ -5,3 +5,7 @@ os.system('python3 ~/conkyConfigs/ListMD/mdToPython.py /home/shafay/conkyConfigs
 os.system('python3 ~/conkyConfigs/ListMD/combineFiles.py /home/shafay/conkyConfigs/ListMD/notesText /home/shafay/conkyConfigs/ListMD/hwText')
 
 # ${execi 3600  }${execi 3600 }${execpi 300  }
+
+# ${hr 1}
+# ${font Entopia:bold:size=12}${color 33E9FF}Notes${alignc}--------------------------------------------------------${alignr}${color red}HW/assignments
+# $font$color${execpi 300 python3 /home/shafay/conkyConfigs/ListMD/execute.py }
